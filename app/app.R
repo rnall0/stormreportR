cc440402bba3394d406cb26ed3d957c86074c77b
Robert Nall <rmn0001@gmail.com>
2018-01-22 18:48:56 -0600

    Make date range input draggable

--- app/app.R
+++ app/app.R
@@ -10,6 +10,7 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("mymap", width = "100%", height = "100%"),
   absolutePanel(top = 230, left = 10,
+		draggable = TRUE,
                 # sliderInput("date_range",
                 #     "Choose a Date Range:",
                 #     min = as.Date("2017-07-01"),
